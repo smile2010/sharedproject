957dd667c62630dee9930901627cef5dd9123ed0
Smile <smile2002@gmail.com>
2018-03-31 18:55:33 +0800

    Initial commit

---
+++ src/main/java/cn/xiao/App.java
@@ -0,0 +1,13 @@
+package cn.xiao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a shared project!" );
+    }
+}
